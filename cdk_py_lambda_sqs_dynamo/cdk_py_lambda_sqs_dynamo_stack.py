428211cb26b9495d90763cb15b68cd11e0eb3c09
raven4ever <wrtv23@gmail.com>
2020-08-31 21:15:26 +0300

    basic deployment

--- cdk_py_lambda_sqs_dynamo/cdk_py_lambda_sqs_dynamo_stack.py
+++ cdk_py_lambda_sqs_dynamo/cdk_py_lambda_sqs_dynamo_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_lambda as lmdb,
+    aws_apigateway as apigw)
 
 
 class CdkPyLambdaSqsDynamoStack(core.Stack):
@@ -6,4 +9,16 @@ class CdkPyLambdaSqsDynamoStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Lambda function
+        lambdaFunction = lmdb.Function(
+            self, 'LSD-Function',
+            runtime=lmdb.Runtime.PYTHON_3_8,
+            code=lmdb.Code.from_asset('lambda'),
+            handler='main.handler',
+        )
+
+        # API GW
+        api = apigw.RestApi(self, 'API-GW')
+        apiLambdaIntegration = apigw.LambdaIntegration(lambdaFunction)
+        apiLambdaResource = api.root.add_resource('gigel')
+        apiLambdaResource.add_method('GET', apiLambdaIntegration)
